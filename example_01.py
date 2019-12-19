7bf168dcf523a0f2d7f633a0d5beb1bfb71ba002
Tony Pernicano <tony.pernicano@us.nks.net>
2019-12-19 16:48:42 -0500

    adds comments to the example_01.py file

--- example_01.py
+++ example_01.py
@@ -14,31 +14,31 @@ def main():
         port=Config.DB_PORT,
         sslmode=Config.DB_SSLMODE)
 
-    #
+    # Create a cursor to work with the database
     cur = conn.cursor()
 
-    #
+    # Creates a simple Table
     cur.execute("CREATE TABLE test (id SERIAL PRIMARY KEY, name VARCHAR, email VARCHAR);")
 
-    #
+    # Inserts a single row into the table
     cur.execute("INSERT INTO test (name, email) VALUES (%s, %s)", ("Tony", "tony@example.com"))
 
-    #
+    # Executes a select on the current table
     cur.execute("SELECT * FROM test;")
 
-    #
+    # Gets the first Item returned from the select
     first = cur.fetchone()
 
-    #
+    # Displays the result
     print(first)
 
-    #
+    # Deletes the table that was created
     cur.execute("DROP TABLE test;")
 
-    #
+    # Finishes the Transaction
     conn.commit()
 
-    #
+    # Closes the Cursor and database connection
     cur.close()
     conn.close()
 

cd9a31313f25a569092b896da81c038c70040feb
Tony Pernicano <tony.pernicano@us.nks.net>
2019-12-30 17:18:31 -0500

    adds some additional code examples to example_01.py

--- example_01.py
+++ example_01.py
@@ -23,20 +23,50 @@ def main():
     # Inserts a single row into the table
     cur.execute("INSERT INTO test (name, email) VALUES (%s, %s);", ("Tony", "tony@example.com"))
 
+    # Inserts another row into the table
+    cur.execute("INSERT INTO test (name, email) VALUES (%s, %s);", ("Sue", "sue@example.com"))
+
     # Executes a select on the current table
     cur.execute("SELECT * FROM test;")
 
-    # Gets the first Item returned from the select
+    # Gets the first row returned from the select
     first = cur.fetchone()
 
+    # Gets the next row
+    second = cur.fetchone()
+
+    # If there are no more rows `None` will be returned
+    third = cur.fetchone()
+
     # Displays the result
-    print(first)
+    print(first)  # `(1, 'Tony', 'tony@example.com')`
+    print(second)  # `(2, 'Sue', 'sue@example.com')`
+    print(third)  # `None`
+
+    # Inserts multiple rows into the table with a single query
+    cur.execute("INSERT INTO test (name, email) VALUES (%s, %s), (%s, %s), (%s, %s);", (
+        "Erin", "erin@example.com",
+        "Doug", "doug@example.com",
+        "Jan", "jan@example.com",
+    ))
+
+    # Executes a select with an order by, limit, and offset
+    cur.execute("SELECT * FROM test ORDER BY id LIMIT 2 OFFSET 2;")
+
+    # Fetch all the results stored in the cursor as a list
+    results = cur.fetchall()
+
+    print(results)  # `[(3, 'Erin', 'erin@example.com'), (4, 'Doug', 'doug@example.com')]`
 
     # Deletes the table that was created
     cur.execute("DROP TABLE test;")
 
-    # Finishes the Transaction
+    # Finishes the Transaction (A new transaction will automatically be created)
     conn.commit()
+
+    # you can also rollback the transaction if something went wrong
+    cur.execute("CREATE TABLE mistake (id SERIAL PRIMARY KEY, blunder VARCHAR, fault VARCHAR);")
+    conn.rollback()
 
     # Closes the Cursor and database connection
     cur.close()
